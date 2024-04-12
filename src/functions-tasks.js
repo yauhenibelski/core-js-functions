b5ca02812a75ffb8ebe97922d22a529020beb2b7
yauhenibelski <108809026+yauhenibelski@users.noreply.github.com>
2024-04-12 11:24:12 +0200

    memoize

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -105,8 +105,9 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const cash = func();
+  return () => cash;
 }
 
 /**
