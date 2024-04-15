ba1c96c8a1f0b28f146bfdaaa382ac5feaac31e5
yauhenibelski <108809026+yauhenibelski@users.noreply.github.com>
2024-04-15 19:51:03 +0200

    feat: getIdGeneratorFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -208,8 +208,12 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let id = startFrom;
+  return () => {
+    id += 1;
+    return id - 1;
+  };
 }
 
 module.exports = {
